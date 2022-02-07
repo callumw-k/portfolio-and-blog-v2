aafd46d9f04a916dea283573490e5a0054b79600
Callum Wellwood-Kane <cwellkane@gmail.com>
2022-02-07 20:17:02 +1100

    removed container as wrapper to allow navbar full width

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,32 +1,34 @@
 import React, { useState } from "react";
-import { Button, Container, Flex } from "@chakra-ui/react";
+import { Box, Button, Container, Flex } from "@chakra-ui/react";
 import MenuLinks from "./MenuLinks";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
   return (
-    <Container maxW="6xl" as="header" boxShadow="lg">
-      <Flex
-        as="nav"
-        align="center"
-        justify="space-between"
-        wrap="wrap"
-        w="100%"
-        py={8}
-      >
-        <h1>Logo</h1>
-        <Button
-          bg="primary_cyan.900"
-          _hover={{ bg: "primary_cyan.800" }}
-          display={{ base: "block", md: "none" }}
-          onClick={toggle}
+    <Box as="header">
+      <Container maxW="6xl">
+        <Flex
+          as="nav"
+          align="center"
+          justify="space-between"
+          wrap="wrap"
+          w="100%"
+          py={8}
         >
-          Menu Button
-        </Button>
-        <MenuLinks isOpen={isOpen} />
-      </Flex>
-    </Container>
+          <h1>Logo</h1>
+          <Button
+            bg="primary_cyan.900"
+            _hover={{ bg: "primary_cyan.800" }}
+            display={{ base: "block", md: "none" }}
+            onClick={toggle}
+          >
+            Menu Button
+          </Button>
+          <MenuLinks isOpen={isOpen} />
+        </Flex>
+      </Container>
+    </Box>
   );
 };
 
